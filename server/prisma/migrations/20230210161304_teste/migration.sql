55926722c79be8012c455cf352dc7776505cd212
StealthWorm <thierrypitela@hotmail.com>
2023-02-10 14:11:04 -0300

    styles: logout styles

---
+++ server/prisma/migrations/20230210161304_teste/migration.sql
@@ -0,0 +1,50 @@
+-- CreateTable
+CREATE TABLE "habits" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "title" TEXT NOT NULL,
+    "created_at" DATETIME NOT NULL,
+    "user_id" TEXT NOT NULL,
+    CONSTRAINT "habits_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "habit_week_days" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "habit_id" TEXT NOT NULL,
+    "week_day" INTEGER NOT NULL,
+    CONSTRAINT "habit_week_days_habit_id_fkey" FOREIGN KEY ("habit_id") REFERENCES "habits" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "days" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "date" DATETIME NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "day_habits" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "day_id" TEXT NOT NULL,
+    "habit_id" TEXT NOT NULL,
+    "user_id" TEXT NOT NULL,
+    CONSTRAINT "day_habits_day_id_fkey" FOREIGN KEY ("day_id") REFERENCES "days" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "day_habits_habit_id_fkey" FOREIGN KEY ("habit_id") REFERENCES "habits" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "day_habits_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "photo" TEXT NOT NULL,
+    "email" TEXT NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "habit_week_days_habit_id_week_day_key" ON "habit_week_days"("habit_id", "week_day");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "days_date_key" ON "days"("date");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "day_habits_day_id_habit_id_user_id_key" ON "day_habits"("day_id", "habit_id", "user_id");
--- web/src/pages/Home.tsx
+++ web/src/pages/Home.tsx
@@ -5,6 +5,7 @@ import { signOut } from "firebase/auth";
 import { auth } from "../lib/firebase"
 import { useContext } from 'react';
 import { AuthContext } from '../contexts/AuthContext';
+import { SignOut, User } from 'phosphor-react';
 
 export function Home() {
   const { currentUser } = useContext(AuthContext);
@@ -12,7 +13,19 @@ export function Home() {
   return (
     <div className="w-full flex justify-center items-center transition-all  bg-[color:var(--bg-color)]">
       <div className="w-full max-w-5xl px-6 flex flex-col md:gap-16 sm:mb-10">
-        <button type='button' onClick={() => signOut(auth)}>Sign out {currentUser?.name} </button>
+        <button type='button' className='absolute top-2 left-10 hover:brightness-150 transition-all' onClick={() => signOut(auth)}>
+          <div className='flex justify-between p-2 gap-4 items-center'>
+            {currentUser?.photo
+              ?
+              <img src={currentUser?.photo} alt="user_photo"  className="flex rounded-full w-8"/>
+              :
+              <User size={24} />
+            }
+
+            <strong>{currentUser?.name}</strong>
+            <SignOut size={24} />
+          </div>
+        </button>
         {currentUser && (
           <>
             <Header />
